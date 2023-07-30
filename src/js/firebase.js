0d2225d90bea0ca2b26d176528a7f1c01f3f1bf7
OmarSherif1998 <omarmohamedsherif98@gmail.com>
2023-07-30 17:24:21 +0300

    firebase update

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/firestore";
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAn0sm-INF40AdMDI1gdDxl9RMYEBu-RjY',
-  authDomain: 'slack-clone-bc6c2.firebaseapp.com',
-  projectId: 'slack-clone-bc6c2',
-  storageBucket: 'slack-clone-bc6c2.appspot.com',
-  messagingSenderId: '976255946391',
-  appId: '1:976255946391:web:e742bcbfe20807cd6be3d2',
+  apiKey: "AIzaSyCykhxWflcfHQk9RdemTdb-nivdI8Bx-qI",
+  authDomain: "slack-clone-d42e5.firebaseapp.com",
+  projectId: "slack-clone-d42e5",
+  storageBucket: "slack-clone-d42e5.appspot.com",
+  messagingSenderId: "154984612823",
+  appId: "1:154984612823:web:e56f30cd800d69c4158192"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
